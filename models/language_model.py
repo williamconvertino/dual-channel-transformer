cd0e6dd94797d5c6f863af9326240eedffe04006
William Convertino <williamconvertino@gmail.com>
2025-04-24 14:26:46 -0400

    Fixed import

--- models/language_model.py
+++ models/language_model.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from model_components import TransformerBlock, DualBlock
+from .model_components import TransformerBlock, DualBlock
 
 class LanguageModel(nn.Module):
     def __init__(self, config):
